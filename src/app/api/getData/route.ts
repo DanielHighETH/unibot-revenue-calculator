3fef4e9b9e4d0111edf835fac400e7714023bcb4
DanielHighETH <danielhigheth@gmail.com>
2023-07-30 11:42:42 +0200

    build error fix

--- src/app/api/getData/route.ts
+++ src/app/api/getData/route.ts
@@ -4,35 +4,42 @@ require('dotenv').config()
 
 export async function GET() {
 
-  const duneRes = await fetch(`https://api.dune.com/api/v1/query/2636251/results?api_key=${process.env.DUNE_API_KEY}`, {
-    cache: 'no-store',
-  })
-  const duneData = await duneRes.json()
+  try {
+    const duneRes = await fetch(`https://api.dune.com/api/v1/query/2636251/results?api_key=${process.env.DUNE_API_KEY}`, {
+      cache: 'no-store',
+    })
+    const duneData = await duneRes.json()
+    
 
-  await fetch(`http://localhost:3000/api/savePrice`, {
-    cache: 'no-store',
-  })
+    await fetch(`http://localhost:3000/api/savePrice`, {
+      cache: 'no-store',
+    })
 
-  const pricesRes = await fetch(`http://localhost:3000/api/getPrice`, {
-    cache: 'no-store',
-  })
-  const pricesData = await pricesRes.json()
-  console.log(pricesData)
-  const unibotPrice = pricesData.data.unibotPrice
-  const ethereumPrice = pricesData.data.ethereumPrice
+    const pricesRes = await fetch(`http://localhost:3000/api/getPrice`, {
+      cache: 'no-store',
+    })
+    const pricesData = await pricesRes.json()
+    console.log(pricesData)
+    const unibotPrice = pricesData.data.unibotPrice
+    const ethereumPrice = pricesData.data.ethereumPrice
 
-  const data = {
-    annualizedCombinedAPY: numeral(duneData.result.rows[0].annualizedCombinedAPY).format('0,0.00'),
-    annualizedCombinedAPR: numeral(duneData.result.rows[0].annualizedCombinedAPR).format('0,0.00'),
-    dailyHolderRevenue: numeral(duneData.result.rows[0].combinedHolderRevenue24hETH).format('0,0.00'),
-    averageDailyVolumeUSD: numeral(duneData.result.rows[0].averageDailyVolumeUSD).format('0,0.00'),
-    unibotMarketcapETH: numeral(duneData.result.rows[0].unibotMarketcapETH).format('0,0.00'),
-    totalNumberOfTransactions: numeral(duneData.result.rows[0].totalNumberOfTransactions).format('0,0'),
-    totalNumberOfUsers: numeral(duneData.result.rows[0].totalNumberOfUsers).format('0,0'),
-    ethereumPrice: numeral(ethereumPrice).format('0,0.00'),
-    unibotPrice: numeral(unibotPrice).format('0,0.00'),
+    const data = {
+      annualizedCombinedAPY: numeral(duneData.result.rows[0].annualizedCombinedAPY).format('0,0.00'),
+      annualizedCombinedAPR: numeral(duneData.result.rows[0].annualizedCombinedAPR).format('0,0.00'),
+      dailyHolderRevenue: numeral(duneData.result.rows[0].combinedHolderRevenue24hETH).format('0,0.00'),
+      averageDailyVolumeUSD: numeral(duneData.result.rows[0].averageDailyVolumeUSD).format('0,0.00'),
+      unibotMarketcapETH: numeral(duneData.result.rows[0].unibotMarketcapETH).format('0,0.00'),
+      totalNumberOfTransactions: numeral(duneData.result.rows[0].totalNumberOfTransactions).format('0,0'),
+      totalNumberOfUsers: numeral(duneData.result.rows[0].totalNumberOfUsers).format('0,0'),
+      ethereumPrice: numeral(ethereumPrice).format('0,0.00'),
+      unibotPrice: numeral(unibotPrice).format('0,0.00'),
+    }
+    
+
+
+    return NextResponse.json({ data })
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return NextResponse.error() 
   }
-
-
-  return NextResponse.json({ data })
 }
--- src/app/api/getPrice/route.ts
+++ src/app/api/getPrice/route.ts
@@ -9,9 +9,18 @@ export async function GET() {
     try {
         const fileContents = await fs.readFile(filePath, 'utf8');
         data = JSON.parse(fileContents);
-        console.log(data)
-    } catch (err) {
+    } catch (err: any) {
         console.error(`Error reading file from disk: ${err}`);
+        
+        if (err.code === 'ENOENT') {
+            console.log('prices.json does not exist. Creating it now.');
+            data = { message: 'This is a newly created file.' };
+
+            await fs.writeFile(filePath, JSON.stringify(data), 'utf8');
+        }
+        else {
+            throw err;
+        }
     }
 
     return NextResponse.json({ data })
