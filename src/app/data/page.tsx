e2b84dfecdf7842654f043e73400a4ec7d515180
DanielHighETH <danielhigheth@gmail.com>
2023-07-30 11:52:41 +0200

    data page delete

--- src/app/data/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Faq() {
-
-    return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2"> 
-        <h1>DATA page</h1>
-    </div>
-    );
-}
