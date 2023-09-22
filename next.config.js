634c09c66f2b1eb391dc5c995611ef5507381032
DanielHighETH <danielhigheth@gmail.com>
2023-09-22 22:26:50 +0700

    redirect fix

--- next.config.js
+++ next.config.js
@@ -7,21 +7,4 @@ const nextConfig = {
 
 module.exports = {
     ...nextConfig,
-    async redirects() {
-        if (process.env.NODE_ENV !== 'development' || process.env.NODE_ENV !== 'production') {
-            return [
-                {
-                    source: '/',
-                    destination: 'https://unibot-revenue-calculator.com/',
-                    permanent: true,
-                },
-                {
-                    source: '/about',
-                    destination: 'https://unibot-revenue-calculator.com/about',
-                    permanent: true,
-                },
-            ];
-        } 
-        return [];
-    }
 }
